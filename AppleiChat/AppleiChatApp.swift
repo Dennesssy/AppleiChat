8a89ee1102e77def96c37d0a8d17f9bdec200b78
Dennesssy <139027232+Dennesssy@users.noreply.github.com>
2025-10-03 06:10:58 -0600

    Initial Commit

---
+++ AppleiChat/AppleiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleiChatApp.swift
+//  AppleiChat
+//
+//  Created by Dennis Stewart Jr. on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleiChatTests/AppleiChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleiChatTests.swift
+//  AppleiChatTests
+//
+//  Created by Dennis Stewart Jr. on 10/3/25.
+//
+
+import Testing
+@testable import AppleiChat
+
+struct AppleiChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
